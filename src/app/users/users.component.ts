fa6f163fc2761569e18cd6eb6acf589f04ceaee5
Smriti-sh <smritisapr2001@gmail.com>
2024-06-19 19:20:26 +0530

    finishing

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -24,7 +24,6 @@ export class UsersComponent implements OnInit {
 
     //for cases when we fetch users from a server that took like 2 seconds to load or something like that
     //and we can do that by using a setTimeout
-<<<<<<< HEAD
       // setTimeout(() => {
   //       //users property wiz an array of user objects
         // this.users = [
@@ -64,47 +63,6 @@ export class UsersComponent implements OnInit {
     
 
     this.showExtended = true;
-=======
-      setTimeout(() => {
-  //       //users property wiz an array of user objects
-        this.users = [
-          {
-            firstname:'john',
-            lastName:'doe',
-            age:30,
-            address:{
-              street:'50 Main St.',
-              city:'Boston',
-              state:'MA'
-            }
-          },
-          {
-            firstname:'kevin',
-            lastName:'johnson',
-            age:34,
-            address:{
-              street:'50 hola St.',
-              city:'cosco',
-              state:'LA'
-            }
-          },
-          {
-            firstname:'phil',
-            lastName:'dunphy',
-            age:40,
-            address:{
-              street:'50 bell St.',
-              city:'xyz',
-              state:'K'
-            }
-          }
-        ];
-        this.loaded = true;
-      },10000);
-    
-
-    this.showExtended = false;
->>>>>>> 2df32bd0f9bd5810094111a37c942495baae680c
 
   //   this.addUser(
   //     {
@@ -118,7 +76,6 @@ export class UsersComponent implements OnInit {
   //     }
   //   });
   // }
-<<<<<<< HEAD
 this.users = [
       {
         firstname:'John',
@@ -166,66 +123,6 @@ this.users = [
 
     this.setCurrentClasses();
     this.setCurrentStyles();
-=======
-// this.users = [
-//       {
-//         firstname:'john',
-//         lastName:'doe',
-//         age:30,
-//         address:{
-//           street:'50 Main St.',
-//           city:'Boston',
-//           state:'MA'
-//         },
-//         image:  'https://fastly.picsum.photos/id/87/200/300.jpg?hmac=YgijkxA35wxtPYqEsxGObDtNAlK3MVmNNb8ZH8IX1Rs',
-//         balance:100,
-//         registered: new Date('18/06/2024 13:30:00')
-//       },
-//       {
-//         firstname:'kevin',
-//         lastName:'johnson',
-//         age:34,
-//         address:{
-//           street:'50 hola St.',
-//           city:'cosco',
-//           state:'LA'
-//         },
-//         image: 'https://picsum.photos/200/300/2',
-//         isActive:true,
-//         balance:100,
-//         registered: new Date('18/06/2024 13:30:00')
-//       },
-//       {
-//         firstname:'phil',
-//         lastName:'dunphy',
-//         age:40,
-//         address:{
-//           street:'50 bell St.',
-//           city:'xyz',
-//           state:'K'
-//         },
-//         image: 'https://fastly.picsum.photos/id/87/200/300.jpg?hmac=YgijkxA35wxtPYqEsxGObDtNAlK3MVmNNb8ZH8IX1Rs',
-//         balance:100,
-//         registered: new Date('18/06/2024 13:30:00')
-//       }
-//     ];
-//     this.loaded = true;
-
-//     this.setCurrentClasses();
-//     this.setCurrentStyles();
->>>>>>> 2df32bd0f9bd5810094111a37c942495baae680c
-
-// this.addUser(
-//   {
-//   firstname:'david',
-//   lastName:'jackson',
-//   age:12,
-//   // address:{
-//   //   street:'50 sun St.',
-//   //   city:'xyz',
-//   //   state:'K'
-//   // }
-// });
 }
 
 setCurrentClasses(){
