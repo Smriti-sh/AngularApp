f2d037cb4a4b1f55ad25831f4201ad6050a147f6
Smriti-sh <smritisapr2001@gmail.com>
2024-06-21 18:26:14 +0530

    Input & Edit State: 38

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { Post } from '../models/Post';
 import { PostService } from '../services/post.service';
 
@@ -8,13 +8,25 @@ import { PostService } from '../services/post.service';
   styleUrl: './post-form.component.css'
 })
 export class PostFormComponent implements OnInit{
+  
+  //Properties
 
-  post:Post[] =[];
-  @Output() newPost: EventEmitter<Post> = new EventEmitter();
+post:Post[] =[];
+  @Output() newPost: EventEmitter<Post> = new EventEmitter();   //@Output(): A decorator that marks the newPost property as an output property, 
+// newPost will emit events carrying Post objects.
+  @Input() currentPost:Post ={
+    id: 0,
+    title: '',
+    body: ''
+  };
+  @Input()
+  isEdit: boolean = false;
 
-  constructor(private postService: PostService){}
+  constructor(private postService: PostService){
+  }
 
   ngOnInit(): void {
+    
   }
 
   addPost(title:string,body:string){
@@ -22,10 +34,17 @@ export class PostFormComponent implements OnInit{
       alert('Please add post');
     } else {
       //savePost typically sends an HTTP POST request to save the new post on the server and returns an Observable<Post>, where Post represents the saved post.
-      this.postService.savePost({title,body} as Post).subscribe(post => {
+      this.postService.savePost({title,body} as Post).subscribe
+      (post => {
+        console.log(this.currentPost, "currentPost");
         //we are emitting an event from this component & it includes the new post
         this.newPost.emit(post);    
       })
     }
+  }
+
+  updatePost(){
+    console.log(123);
+    
   }
 }
